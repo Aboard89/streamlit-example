44a4fbf9fc824fb49162f17ec86b01571b06355b
Aboard89 <81583324+Aboard89@users.noreply.github.com>
2024-04-09 14:45:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,20 @@ def load_data():
 data = load_data()
 
 # Sidebar - Year selection
-selected_year = st.sidebar.selectbox('Select a Year', data['Year'].unique())
+selected_year = st.sidebar.selectbox('Select a Year', data['year'].unique())
 
 # Sidebar - Location selection based on the year
 filtered_data_by_year = data[data['Year'] == selected_year]
-selected_location = st.sidebar.selectbox('Select a Location', filtered_data_by_year['Location'].unique())
+selected_location = st.sidebar.selectbox('Select a Location', filtered_data_by_year['location'].unique())
 
 # Filtering data based on selection
-filtered_data = filtered_data_by_year[filtered_data_by_year['Location'] == selected_location]
+filtered_data = filtered_data_by_year[filtered_data_by_year['location'] == selected_location]
 
 # Get race winner
-race_winner = filtered_data[filtered_data['Race Winner'] == 1]['Driver Name'].iloc[0]
+race_winner = filtered_data[filtered_data['race_win'] == 1]['driver_name'].iloc[0]
 
 # Get predicted race winner
-predicted_winner = filtered_data[filtered_data['Predicted Winner']].iloc[0]['Driver Name']
+predicted_winner = filtered_data[filtered_data['Predicted_Winner']].iloc[0]['driver_name']
 
 # Display the results
 st.write(f"Actual Race Winner for {selected_year} in {selected_location}: {race_winner}")
