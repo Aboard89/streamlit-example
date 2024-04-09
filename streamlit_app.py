76e1a203988d09c5a4779603429e7a98f57fcbc0
Aboard89 <81583324+Aboard89@users.noreply.github.com>
2024-04-09 14:48:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,20 @@ selected_location = st.sidebar.selectbox('Select a Location', filtered_data_by_y
 filtered_data = filtered_data_by_year[filtered_data_by_year['location'] == selected_location]
 
 # Get race winner
-race_winner = filtered_data[filtered_data['race_win'] == 1]['driver_name'].iloc[0]
+race_winner_data = filtered_data[filtered_data['race_win'] == 1]
+if not race_winner_data.empty:
+    race_winner = race_winner_data['driver_name'].iloc[0]
+else:
+    race_winner = 'No winner data'
 
 # Get predicted race winner
-predicted_winner = filtered_data[filtered_data['Predicted_Winner']].iloc[0]['driver_name']
+predicted_winner_data = filtered_data[filtered_data['predicted_win']]
+if not predicted_winner_data.empty:
+    predicted_winner = predicted_winner_data['driver_name'].iloc[0]
+else:
+    predicted_winner = 'No predicted winner data'
 
 # Display the results
 st.write(f"Actual Race Winner for {selected_year} in {selected_location}: {race_winner}")
 st.write(f"Predicted Race Winner for {selected_year} in {selected_location}: {predicted_winner}")
+
