c4f858f4a9c5bda97a944c20e248998fafcd5171
Aboard89 <boardman1989@gmail.com>
2024-04-15 18:57:51 +0100

    shap

--- pages/3. Shapley Values.py
+++ pages/3. Shapley Values.py
@@ -13,38 +13,34 @@ best_pipeline = load_model()
 rf_model = best_pipeline.named_steps['clf']
 scaler = best_pipeline.named_steps['scl']
 
-# Load SHAP values and feature names
 shap_values_df = pd.read_csv('shap_values.csv', encoding='ISO-8859-1')
-
-# Check if 'output' column exists and drop it if it does
 if 'output' in shap_values_df.columns:
     shap_values_df = shap_values_df.drop(columns=['output'])
 feature_names = shap_values_df.columns
 
 st.title('F1 Race Prediction SHAP Value Plot')
 
-# Input for index number
 index_number = st.number_input('Enter the index number from the F1 Race prediction app:', min_value=0, format='%d')
 
 if st.button('Generate SHAP Plot'):
-    try:
-        # Assuming index number is used as the row index in shap_values_df
-        driver_shap_values = shap_values_df.iloc[index_number].values.reshape(1, -1)
-        driver_features_transformed = scaler.transform(driver_shap_values)
-        explainer = shap.TreeExplainer(rf_model)
-        shap_values = explainer.shap_values(driver_features_transformed)
+    if index_number >= len(shap_values_df):
+        st.error(f"Index is out of bounds. Please enter a number between 0 and {len(shap_values_df)-1}.")
+    else:
+        try:
+            driver_shap_values = shap_values_df.iloc[index_number].values.reshape(1, -1)
+            driver_features_transformed = scaler.transform(driver_shap_values)
+            explainer = shap.TreeExplainer(rf_model)
+            shap_values = explainer.shap_values(driver_features_transformed)
 
-        # Plot
-        force_plot = shap.force_plot(
-            explainer.expected_value[1], 
-            shap_values[1],
-            driver_features_transformed,
-            feature_names=feature_names
-        )
+            force_plot = shap.force_plot(
+                explainer.expected_value[1], 
+                shap_values[1],
+                driver_features_transformed,
+                feature_names=feature_names
+            )
 
-        # Convert the plot to HTML and display
-        shap_html = f"<head>{shap.getjs()}</head><body>{force_plot.html()}</body>"
-        st.components.v1.html(shap_html, height=300)
+            shap_html = f"<head>{shap.getjs()}</head><body>{force_plot.html()}</body>"
+            st.components.v1.html(shap_html, height=300)
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
