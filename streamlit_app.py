c61800bedcc37853e52382f6c35a18b9f0cd8bb8
Aboard89 <boardman1989@gmail.com>
2024-04-15 19:26:03 +0100

    main app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # app.py or streamlit_app.py
 import streamlit as st
-from pages import f1_2024_race_predictor, f1_2024_top_driver, Shapley Values, project_conclusions  # Assumes 'race_winner.py' and 'top_3_drivers.py' are inside a 'pages' folder
+from pages import f1_2024_race_predictor, f1_2024_top_driver, Shapley_Values, project_conclusions  # Assumes 'race_winner.py' and 'top_3_drivers.py' are inside a 'pages' folder
 
 PAGES = {
     "Top 3 Drivers": f1_2024_race_predictor,
